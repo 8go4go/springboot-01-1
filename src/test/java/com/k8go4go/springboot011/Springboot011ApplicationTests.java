f24492fcd396a9774fba20cb35eaffc3b28d9e19
leesangwook <8go4go@gmail.com>
2024-05-02 17:47:36 +0900

    Initial commit

---
+++ src/test/java/com/k8go4go/springboot011/Springboot011ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.k8go4go.springboot011;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot011ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
